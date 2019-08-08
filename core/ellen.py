a41fd25cfc53e8bc231669c96f27f2223948f5fd
Ryan Kwon <rynkwn@gmail.com>
2019-08-08 01:22:35 -0700

    Some tidbits.

--- core/ellen.py
+++ core/ellen.py
@@ -1,2 +1,11 @@
 # Even though my day job is Python, 
 # I don't remember how to write Python without some reference code.
+
+from typing import (
+	Union,
+	Text,
+)
+
+def learn(key: Text, textblob: Text) -> bool:
+
+	pass
